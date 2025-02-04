39e6e74d0dd70c34b88a51231c1d5fc29f334b9d
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-02-04 21:09:36 +0530

    update getDayOfWeek

--- Week-1/Condition/getDayOfWeek.js
+++ Week-1/Condition/getDayOfWeek.js
@@ -1,14 +1,22 @@
 // You just need to implement the getDayOfWeek function
 function getDayOfWeek(day) {
-    // Return the corresponding day of the week based on the input number
-    switch(day) {
-      case "1": return "Monday"
-      case "2": return "Tuesday"
-      case "3": return "Wednesday"
-      case "3": return "Thursday"
-      case "4": return "Friday"
-      case "5": return "Saturday"
-      case "6": return "Sunday"
-      default: return "Invalid Day"
-    }
+  // Return the corresponding day of the week based on the input number
+  switch(day) {
+    case "1": 
+     return "Monday"
+    case "2": 
+     return "Tuesday"
+    case "3": 
+     return "Wednesday"
+    case "4": 
+     return "Thursday"
+    case "5": 
+     return "Friday"
+    case "6": 
+     return "Saturday"
+    case "7": 
+     return "Sunday"
+    default: 
+     return "Invalid Day"
   }
+}
