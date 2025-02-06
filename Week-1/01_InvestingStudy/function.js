d1d62524f7b22eabb598697ddd917c7819a1b842
RaviRajBhardwaj <ravirajbhardwaaj@gmail.com>
2025-02-06 17:19:03 +0530

    function

---
+++ Week-1/01_InvestingStudy/function.js
@@ -0,0 +1,24 @@
+// fuction is also treat a value like string, number and etc
+
+// function defination below
+
+function sayHi() {
+    console.log("Hi")
+}
+
+// console.log(sayHi) // type of -> function and typeof sayHi -> [Function: sayHi] 
+
+// function expression below
+
+let name = "Ravi";
+
+let greet = function (name) {
+    console.log(name)
+}
+
+// console.log(greet) // type of -> function and typeof greet -> [Function: greet]
+
+let func = greet // func ko greet defination ka reference diya gya hai
+
+func()
+greet()
